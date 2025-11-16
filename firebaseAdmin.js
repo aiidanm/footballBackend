5c181b5261afcbcd0cef151daab340357fac387f
Aidan Murray <iiiaiid4niii@gmail.com>
2025-11-16 22:12:42 +0000

    added file root for secret file in render

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const serviceAccount = JSON.parse(
-  readFileSync(path.join(__dirname, "./creds.json"))
+  readFileSync(path.join(__dirname, "./etc/secrets/creds.json"))
 );
 
 admin.initializeApp({
